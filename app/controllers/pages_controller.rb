a7a0d788a6b4c7c49e22ded03b536cda216f38b3
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-13 14:08:58 +0200

    set up pages controller to store timestamp, user_id and path in cookie

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,26 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :contact]
 
   def home
+    cookies[:timestamp] = Time.now
+    cookies[:user_id] = current_user.id
+    cookies[:url] = '/'
+
+    show_cookies
   end
 
   def contact
+    cookies[:timestamp] = Time.now
+    cookies[:user_id] = current_user.id
+    cookies[:url] = '/contact'
+
+    show_cookies
+  end
+
+  private
+
+  def show_cookies
+    @timestamp = cookies[:timestamp]
+    @user_id = cookies[:user_id]
+    @url = cookies[:url]
   end
 end
